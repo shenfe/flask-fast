9078eb733c8ee6ff3ba47f93a0f45e1710252f03
shenke <shenke@bytedance.com>
2018-06-10 13:12:51 +0800

    fix
    Change-Id: I709f1570a3a93283fbed8d0b29ae52d90e1fc740

--- app/client/__init__.py
+++ app/client/__init__.py
@@ -7,11 +7,12 @@ from flask import Blueprint, render_template
 client_bp = Blueprint('client_app', __name__,
                       url_prefix='',
                       static_url_path='',
-                      static_folder='./vue_app/dist',
-                      template_folder='./vue_app/dist',
+                      static_folder='./dist',
+                      template_folder='./dist',
                       )
 
 
 @client_bp.route('/')
 def index():
     return render_template('index.html')
+
