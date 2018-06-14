774a40ec765f5ad89008db232d3adec482d9e3e2
Shen <shenke@bytedance.com>
2018-06-14 22:42:10 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,38 @@
 #!/usr/bin/env bash
 
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -m|--mode)
+    MODE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -d|--dev)
+    MODE="Development"
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
 
 cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${cwd}
 
-export FLASK_CONFIG="Production"
-python ./run.py
+
+if [ "$MODE" != "Development" ]; then
+    export FLASK_CONFIG="Production"
+    echo ">>> Production"
+else
+    echo ">>> Development"
+fi
+
+python ./run.py & . ./post_run
