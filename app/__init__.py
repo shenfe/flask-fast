6e699c1c252bde882e55dbfd7c9d75046e90567d
Shen <shenke@bytedance.com>
2018-06-14 22:36:08 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,6 @@ from flask_cas import CAS
 from flask_cors import CORS
 import click
 
-from .api import api_bp
-from .client import client_bp
-
 from .config import current_mode
 
 
@@ -16,9 +13,13 @@ app.config.from_object('app.config.{}'.format(app_mode))
 app.logger.info('>>> {}'.format(app_mode))
 
 # CAS Config
-app.config['CAS_SERVER'] = 'https://sso.bytedance.com/'
+app.config['CAS_SERVER'] = 'https://sso.your-server.com/'
+app.config['CAS_LOGIN_ROUTE'] = '/cas/login'
 app.config['CAS_AFTER_LOGIN'] = '/'
-CAS(app)
+cas = CAS(app, '/cas')
+
+from .api import api_bp
+from .client import client_bp
 
 # Blueprints
 if app_mode == 'Development': # Use this way to know Production or Development
